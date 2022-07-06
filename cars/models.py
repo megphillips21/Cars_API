75b31938cf6fa0f07608f4e94523ed70fab015a4
Megan Phillips <megphillips21@gmail.com>
2022-07-06 16:59:42 -0700

    created dealerships app, dealership model, added dealership to car model. Ran into error opening in django admin site for dealership

--- cars/models.py
+++ cars/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from dealerships.models import Dealership
 
 # Create your models here.
   
@@ -6,4 +7,5 @@ class Car(models.Model):
     make = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
     year = models.IntegerField()
-    price = models.DecimalField( max_digits=8, decimal_places=2) 
+    price = models.DecimalField( max_digits=8, decimal_places=2)
+    dealership = models.ForeignKey(Dealership, on_delete=models.CASCADE, null=True)
--- cars_project/settings.py
+++ cars_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'cars'
+    'cars',
+    'dealerships',
 ]
 
 MIDDLEWARE = [
